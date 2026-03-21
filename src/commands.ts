cb5aa0a3fa97915ea823105732635d03010b13de
Alex <130986014+AylexCODE@users.noreply.github.com>
2026-03-21 22:57:50 +0800

    Cleaning

--- src/commands.ts
+++ src/commands.ts
@@ -8,23 +8,15 @@ export default function registerAllCommands(context: ExtensionContext) {
         try{
             if(!uris || uris.length === 0) return window.showWarningMessage('Please select one or more files or directories.');
             copy(uris);
-        }catch{
-            window.showErrorMessage('Error selecting directory.');
-        }
+        }catch{window.showErrorMessage('Error selecting directory.')}
     }));
-    context.subscriptions.push(commands.registerCommand('cloudclipboard.editorCopy', () => {
-        copy(undefined);
-    }));
+    context.subscriptions.push(commands.registerCommand('cloudclipboard.editorCopy', () => {copy(undefined)}));
     context.subscriptions.push(commands.registerCommand('cloudclipboard.paste', async(uri?: Uri) => {
         try{
             const dir = await getDirectory(uri);
             if(!dir) return window.showWarningMessage('Please select a directory.');
             paste(dir);
-        }catch{
-            window.showErrorMessage('Error selecting directory.');
-        }
+        }catch{window.showErrorMessage('Error selecting directory.')}
     }));
-    context.subscriptions.push(commands.registerCommand('cloudclipboard.editorPaste', () => {
-        paste(undefined);
-    }));
+    context.subscriptions.push(commands.registerCommand('cloudclipboard.editorPaste', () => {paste(undefined)}));
 }
