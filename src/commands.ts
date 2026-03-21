8719c606bb04cdc4bd457401990c3ed80989d999
Alex <130986014+AylexCODE@users.noreply.github.com>
2026-03-21 17:06:37 +0800

    Working paste command

--- src/commands.ts
+++ src/commands.ts
@@ -1,12 +1,19 @@
-import { commands, type ExtensionContext } from "vscode";
+import { commands, Uri, window, type ExtensionContext } from "vscode";
 import paste from "./commands/paste";
 import copy from "./commands/copy";
+import getDirectory from "./utils/getDirectory";
 
 export default function registerAllCommands(context: ExtensionContext) {
-    context.subscriptions.push(commands.registerCommand('cloudclipboard.copy', () => {
+    context.subscriptions.push(commands.registerCommand('cloudclipboard.copy', async(uri: Uri) => {
         copy();
     }));
-    context.subscriptions.push(commands.registerCommand('cloudclipboard.paste', () => {
-        paste();
+    context.subscriptions.push(commands.registerCommand('cloudclipboard.paste', async(uri?: Uri) => {
+        try{
+            const dir = await getDirectory(uri);
+            if(!dir) return window.showWarningMessage('Select a directory first.');
+            paste(dir);
+        }catch{
+            window.showErrorMessage('Error selecting directory.');
+        }
     }));
 }
--- src/commands/paste.ts
+++ src/commands/paste.ts
@@ -1,5 +1,39 @@
-import { window } from "vscode";
+import { Uri, window, workspace } from "vscode";
+import getConnections from "../utils/getConnections";
+import getDirectory from "../utils/getDirectory";
+import path = require("path");
+import getClipboardContent from "../utils/getClipboardContent";
 
-export default function paste(){
-    window.showInformationMessage('Cloud Clipboard | Paste');
+export default async function paste(dir: string) {    
+    try{
+        const config: string = workspace.getConfiguration("cloudclipboard").get<string>("configuration")!;
+
+        if(config.trim().length === 0) {
+            window.showWarningMessage("Clipboard API Endpoint is not set. Please configure it in the extension settings.");
+            return;
+        }
+
+        const items = (await getConnections(config)).split("\n").filter(Boolean).map((conn) => {
+            return { label: conn };
+        });
+
+        const connection = await window.showQuickPick(items, {
+            canPickMany: false,
+            title: "Select Clipboard"
+        });
+
+        if(connection?.label){
+            const clipboard = await getClipboardContent(config, connection.label);
+            const newDir = path.join(dir, "cloud clipboard");
+
+            await workspace.fs.createDirectory(Uri.file(path.join(dir, "cloud clipboard")));
+            await workspace.fs.writeFile(Uri.file(path.join(newDir, "clipboard.txt")), Buffer.from(clipboard, 'utf-8'));
+
+            window.showInformationMessage(`Pasted ${connection.label} at ${newDir}`);
+        }else{
+            window.showWarningMessage('Paste cancelled.');
+        }
+    }catch{
+        window.showErrorMessage('Error pasting.');
+    }
 }
